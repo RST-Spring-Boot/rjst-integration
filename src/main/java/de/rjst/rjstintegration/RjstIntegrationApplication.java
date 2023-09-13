c54b59da075a20e54199ff5ba6a90850885148c6
RStenzhorn <robert.stenzhorn@outlook.de>
2023-09-13 21:47:02 +0200

    Spring Integration try

--- src/main/java/de/rjst/rjstintegration/RjstIntegrationApplication.java
+++ src/main/java/de/rjst/rjstintegration/RjstIntegrationApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.config.EnableIntegrationManagement;
+import org.springframework.integration.http.config.EnableIntegrationGraphController;
 
 @SpringBootApplication
 @EnableIntegration
 @EnableFeignClients
+@EnableIntegrationManagement
+@EnableIntegrationGraphController
 public class RjstIntegrationApplication {
 
     public static void main(String[] args) {
