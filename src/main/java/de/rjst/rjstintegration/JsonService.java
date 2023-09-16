b21755c1c9d842d2eb0ebb3cb6f745e0b4560fbd
RStenzhorn <robert.stenzhorn@outlook.de>
2023-09-16 17:53:30 +0200

    Spring Integration try

--- src/main/java/de/rjst/rjstintegration/JsonService.java
+++ src/main/java/de/rjst/rjstintegration/JsonService.java
@@ -1,10 +1,11 @@
 package de.rjst.rjstintegration;
 
+import de.rjst.rjstintegration.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "json", url = "https://json-api-service.rjst.de/data")
+@FeignClient(value = "json", url = "https://json-api-service.rjst.de/data", configuration = FeignConfig.class)
 public interface JsonService {
 
     @PostMapping
